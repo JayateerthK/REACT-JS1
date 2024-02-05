5f8f99157da5483b0c8d96853b426f0a8ad52cc5
JayateerthK <jayateerthkakhandki@gmail.com>
2024-02-05 16:19:32 +0530

    new commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const root = document.querySelector("#root");
+const heading = document.querySelector("h1");
 root.innerHTML= "Hello World";
-document.appendChild(root);
+heading.innerHTML = "The test line is working";
+document.appendChild(root, heading);
