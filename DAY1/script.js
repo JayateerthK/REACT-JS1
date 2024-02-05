ff88a3f5c80cc16499e12192a40737e4293ec97a
JayateerthK <jayateerthkakhandki@gmail.com>
2024-02-05 16:26:45 +0530

    This a new change

--- DAY1/script.js
+++ DAY1/script.js
@@ -1,5 +1,5 @@
 const root = document.querySelector("#root");
 const heading = document.querySelector("h1");
-root.innerHTML= "Hello World";
+root.innerHTML= "Hello World!";
 heading.innerHTML = "The test line is working";
 document.appendChild(root, heading);
